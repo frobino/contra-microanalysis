e68f9a162a16d295840625daa81ea3f943ebe0e5
frobino <frobino@users.noreply.github.com>
2024-01-31 15:49:57 +0100

    Initial commit
    Result of the mvn archetype:generate command

---
+++ src/main/java/org/frobino/contra/kernel/App.java
@@ -0,0 +1,13 @@
+package org.frobino.contra.kernel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
