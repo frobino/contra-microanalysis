dc0dd187edf1e46dfe8aa17f372fd9a9d5fdfdc0
frobino <frobino@users.noreply.github.com>
2024-02-02 09:22:46 +0100

    App can now print events using CTFTraceReader

--- src/main/java/org/frobino/contra/kernel/App.java
+++ src/main/java/org/frobino/contra/kernel/App.java
@@ -1,13 +1,31 @@
 package org.frobino.contra.kernel;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.eclipse.tracecompass.ctf.core.CTFException;
+import org.eclipse.tracecompass.ctf.core.trace.CTFTrace;
+import org.eclipse.tracecompass.ctf.core.trace.CTFTraceReader;
+
+public class App {
+	public static void main(String[] args) {
+	    final String lttngUstTracePath = "resources/traces/lttng-ust";
+	    try {
+			CTFTrace trace = new CTFTrace(lttngUstTracePath);
+			CTFTraceReader traceReader = new CTFTraceReader(trace);
+			int counter = 0;
+			while (traceReader.hasMoreEvents()) {
+				System.out.println("Event " + counter + ":");
+				System.out.println("Event name: " + traceReader.getCurrentEventDef().getDeclaration().getName());
+				System.out.println("Timestamp: " + traceReader.getCurrentEventDef().getTimestamp());
+				System.out.println("Events fields: " + traceReader.getCurrentEventDef().getFields());
+				System.out.println();
+				traceReader.advance();
+				counter++;
+			}
+			// Close the reader
+			traceReader.close();
+		} catch (CTFException e) {
+			// Issues when creating new CTFTrace
+			e.printStackTrace();
+		}
+
+	}
 }
