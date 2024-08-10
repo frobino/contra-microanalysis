48702032010cc505dd2ac855d7e3760d20e15e82
frobino <frobino@users.noreply.github.com>
2024-08-10 17:14:52 +0200

    Refactor streamlit script to reuse SQL queries
    Make it ready to reuse/import SQL files and then
    massage the data inside the py script, instead of
    modifying the query

--- contra-kernel/charting/main.py
+++ contra-kernel/charting/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import psycopg2 as p2
 import plotly.express as px
 import plotly.figure_factory as ff
+import random
 
 # Initialize connection.
 # conn = st.connection("postgresql", type="sql")
@@ -22,29 +23,69 @@ params = {
 }
 conn = p2.connect(**params, dbname= "intervals")
 
-# df = conn.query('SELECT * FROM intervalsv2 LIMIT 50;', ttl="10m")
-
-SQL_Query = pd.read_sql('SELECT quark,quark FROM intervalsv2', conn)
-
 st.write("My First App")
-df = pd.DataFrame(SQL_Query, columns=['quark','quark'])
-st.line_chart(df)
+
+# SQL_Query = pd.read_sql('SELECT quark,quark FROM intervalsv2', conn)
+# df = pd.DataFrame(SQL_Query, columns=['quark','quark'])
+# st.line_chart(df)
 
 SQL_Query = pd.read_sql(
 """SELECT
-    (lower(duration)) AS "Start",
-    (upper(duration)) AS "Finish",
-    (SELECT value FROM intervalsv2 WHERE attribute = \'Threads/\' || intervals.value || \'/Exec_name\' LIMIT 1) AS "CPU Task"
+    (lower(duration)) AS "Start time",
+    (upper(duration)) AS "End time",
+    (SELECT value FROM intervalsv2 WHERE attribute = \'Threads/\' || intervals.value || \'/Exec_name\' LIMIT 1) AS "Thread"
 FROM
     intervalsv2 intervals -- <table name> <db name>
 WHERE
     attribute = \'CPUs/1/Current_thread\'
-LIMIT 50""", conn)
+""", conn)
+# LIMIT 50""", conn)
 
 df = pd.DataFrame(SQL_Query)
-df.dropna(subset=['CPU Task'], inplace=True)
-df['Task'] = '1'
-print(df)
+
+# Massage the dataframe to simplify creation of charts
+#
+# Remove 'None' intervals (a.k.a. null intervals/segments)
+df.dropna(subset=['Thread'], inplace=True)
+# NOTE: adding the 'delta' column to enable px.timeline to handle properly int/float x-axis instead of date
+# See:
+# https://community.plotly.com/t/gantt-chart-integer-float-x-axis-labels-instead-of-date/37233/2
+# https://stackoverflow.com/questions/66078893/plotly-express-timeline-for-gantt-chart-with-integer-xaxis
+df['delta'] = df['End time'] - df['Start time']
+
+# Something similar to TC Kernel Control Flow View, using plotly express timeline
+fig = px.timeline(df, x_start="Start time", x_end="End time", y="Thread")
+fig.layout.xaxis.type = 'linear'
+fig.data[0].x = df.delta.tolist()
+fig = fig.full_figure_for_development(warn=False)
+st.plotly_chart(fig, use_container_width=True)
+
+# Massage the dataframe to simplify creation of charts
+#
+df['CPU nr'] = '1'
+
+# The TC Kernel Resource View, using plotly express timeline (not fully working due to issue in px)
+# The line below should work, but some issues are present in px.timeline.
+# See: https://stackoverflow.com/questions/68500434/bars-of-plotly-timeline-disappear-when-adding-color
+# fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="CPU Task")
+fig = px.timeline(df, x_start="Start time", x_end="End time", y="CPU nr")
+fig.update_yaxes(autorange="reversed")
+fig.layout.xaxis.type = 'linear'
+fig.data[0].x = df.delta.tolist()
+fig = fig.full_figure_for_development(warn=False)
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+
+# print(df)
 
 
 # NO: https://stackoverflow.com/questions/73247210/how-to-plot-a-gantt-chart-using-timesteps-and-not-dates-using-plotly
@@ -52,27 +93,21 @@ print(df)
 # fig.update_layout(xaxis_type='linear', autosize=False, width=800, height=400)
 # st.plotly_chart(fig, use_container_width=True)
 
+
+# Massage the dataframe to simplify creation of charts
+#
+# For ff.create_gantt to work, the columns in the dataframe must include the following keys: Task, Start, Finish
+df = df.rename(columns={"CPU nr": "Task", "Start time": "Start", "End time": "Finish"})
+
+# For ff.create_gantt to work with many colors, we need to have a own colors method.
+# See https://stackoverflow.com/questions/55965633/how-to-specify-additional-colors-in-plotly-gantt-chart
+r = lambda: random.randint(0,255)
+colors = ['#%02X%02X%02X' % (r(),r(),r())]
+for i in range(1, df.Thread.nunique()+1):
+    colors.append('#%02X%02X%02X' % (r(),r(),r()))
+
 # The TC Kernel Resource View, using the deprecated create_gantt instead of plotly express timeline:
-fig = ff.create_gantt(df, index_col = 'CPU Task',  bar_width = 0.4, show_colorbar=True, group_tasks=True)
+fig = ff.create_gantt(df, colors=colors, index_col = 'Thread',  bar_width = 0.4, show_colorbar=True, group_tasks=True)
 fig.update_layout(xaxis_type='linear', autosize=False, width=800, height=400)
 st.plotly_chart(fig, use_container_width=True)
 
-# Something similar to TC Kernel Control Flow View, using plotly express timeline
-df['delta'] = df['Finish'] - df['Start']
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="CPU Task")
-fig.layout.xaxis.type = 'linear'
-fig.data[0].x = df.delta.tolist()
-fig = fig.full_figure_for_development(warn=False)
-st.plotly_chart(fig, use_container_width=True)
-
-# The TC Kernel Resource View, using plotly express timeline (not fully working due to issue in px)
-df['delta'] = df['Finish'] - df['Start']
-# The line below should work, but some issues are present in px.timeline.
-# See: https://stackoverflow.com/questions/68500434/bars-of-plotly-timeline-disappear-when-adding-color
-# fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="CPU Task")
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
-fig.update_yaxes(autorange="reversed")
-fig.layout.xaxis.type = 'linear'
-fig.data[0].x = df.delta.tolist()
-fig = fig.full_figure_for_development(warn=False)
-st.plotly_chart(fig, use_container_width=True)
